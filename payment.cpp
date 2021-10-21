91092aac9be2ffb57c1e152e037c047ce1d2a9fe
Sa1m0nTW <komissarovsemen@Mail.ru>
2021-10-21 23:17:52 +0300

    Update payment.cpp

--- payment.cpp
+++ payment.cpp
@@ -1,4 +1,13 @@
-void paymen(vector<Menu>& cart) {
+#include <iostream>
+#include <vector>
+#include <string>
+#include<fstream>
+#include<product.cpp>
+#include<choice.cpp>
+#include<cart.cpp>
+#include<payment.cpp>
+#include<showcart.cpp>
+void paymen(vector<Product>& cart) {
     int sum = 0;
     //Here all goods in cart are withdrawn
     cout << "Amount to pay: " << sum << endl;;
