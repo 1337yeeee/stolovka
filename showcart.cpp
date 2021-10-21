5a6d1b8f72e4e45497a8fb462f676bbc619d609c
Sa1m0nTW <komissarovsemen@Mail.ru>
2021-10-21 23:17:49 +0300

    Update showcart.cpp

--- showcart.cpp
+++ showcart.cpp
@@ -1,4 +1,13 @@
-void showcart(vector<Menu>& cart) {
+#include <iostream>
+#include <vector>
+#include <string>
+#include<fstream>
+#include<product.cpp>
+#include<choice.cpp>
+#include<cart.cpp>
+#include<payment.cpp>
+#include<showcart.cpp>
+void showcart(vector<Product>& cart) {
     int pay;
     //Here information from cart is withdrawn
     cout << "Whould you like to pay the order?\n1.Yes\n2.No\n";
