44edb4b223e792c5f65c2120fd30d2c280094c31
Anatoly <none>
2021-10-22 17:37:09 +0300

    Product is remastered: added constructor wothout parameters; removed func that should hade dealed with massives to append them; added void debugPrint(); removed int main()

--- product.cpp
+++ product.cpp
@@ -1,51 +1,42 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
 using namespace std;
 
 
 
 class Product {
-public:
+private:
 	string name;
 	int price;
 	int id;
-
+public:
+	Product() {
+		;
+	}
 
 	Product(string _name, int _price, int _id=0) {
-		name = _name;
-		price = _price;
-		id = _id;
-	}
-
-//dobavlyaet object klassa Product v konets massiva
-	void addProduct(vector<Product> &massive, Product object) {
-		massive.push_back(object);
-	}
-
-//sozdaet object klassa Product i dobavlyaet v konec massiva
-	void addProduct(vector<Product> &massive, string _name, int _price, int _id=0) {
-		Product object(_name, _price, _id);
-		massive.push_back(object);
+		this->name = _name;
+		this->price = _price;
+		this->id = _id;
 	}
 
 //udalyaet product po ego id
-	void deleteProduct(vector<Product> &massive, Product object) {
+	void deleteProduct(vector<Product> &massive) {
 		for(int i=0; i<massive.size(); i++)
-			if(massive[i] == object)
+			if(massive[i].id == this->id)
 				massive.erase(massive.cbegin()+i);
 	}
 
-//if object a is equal to object b returns 1 else 0
+//if object a.id is equal to object b.id returns 1 else 0
 	int operator == (Product object) {
 		if(this->id == object.id) return 1;
 		else return 0;
 	}
+
+	void debugPrint() {
+		cout<<this->name;
+		cout<<" "<<this->price;
+		cout<<" "<<this->id<<endl;
+	}
 };
-
-
-int main() {
-	//there is nothing
-	return 0;
-}
